0f634154714a2670a6ac9a8d55872a89290c7d54
tejass23-sar <sardetejas92@gmail.com>
2025-04-23 13:59:12 +0530

    Update FootballStandingsApplicationTests.java

--- backend/football-standings/src/test/java/com/football/football_standings/FootballStandingsApplicationTests.java
+++ backend/football-standings/src/test/java/com/football/football_standings/FootballStandingsApplicationTests.java
@@ -3,6 +3,7 @@ package com.football.football_standings;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import com.football.standings.FootballStandingsApplication;
 
 @SpringBootTest
 @ContextConfiguration(classes = FootballStandingsApplication.class)  // Explicitly specify the main class
